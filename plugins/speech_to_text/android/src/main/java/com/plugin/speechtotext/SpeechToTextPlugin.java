62fbb8e533c00b6067e8b7fa1eb3bb95a1320291
rahulrajput19614-prog <rahulrajput19614@gmail.com>
2025-08-12 12:29:37 +0530

    Create SpeechToTextPlugin.java

---
+++ plugins/speech_to_text/android/src/main/java/com/plugin/speechtotext/SpeechToTextPlugin.java
@@ -0,0 +1,16 @@
+package com.plugin.speechtotext;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+
+public class SpeechToTextPlugin implements FlutterPlugin {
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        // No-op
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        // No-op
+    }
+}
