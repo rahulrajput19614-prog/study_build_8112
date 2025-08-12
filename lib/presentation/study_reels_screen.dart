37ca2541aa3edece5532defcec61272c0d2ad6b7
rahulrajput19614-prog <rahulrajput19614@gmail.com>
2025-08-12 08:28:41 +0530

    Create study_reels_screen.dart

---
+++ lib/presentation/study_reels_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class StudyReelsScreen extends StatefulWidget {
+  @override
+  _StudyReelsScreenState createState() => _StudyReelsScreenState();
+}
+
+class _StudyReelsScreenState extends State<StudyReelsScreen> {
+  final List<String> reelUrls = [
+    'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+    'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+  ];
+
+  late PageController _pageController;
+  List<VideoPlayerController> _videoControllers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    for (var url in reelUrls) {
+      final controller = VideoPlayerController.network(url)
+        ..initialize().then((_) {
+          setState(() {});
+        });
+      _videoControllers.add(controller);
+    }
+    _videoControllers[0].play();
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _videoControllers) {
+      controller.dispose();
+    }
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void onPageChanged(int index) {
+    for (var controller in _videoControllers) {
+      controller.pause();
+    }
+    _videoControllers[index].play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        controller: _pageController,
+        itemCount: reelUrls.length,
+        onPageChanged: onPageChanged,
+        itemBuilder: (context, index) {
+          final controller = _videoControllers[index];
+          return Stack(
+            children: [
+              Center(
+                child: controller.value.isInitialized
+                    ? AspectRatio(
+                        aspectRatio: controller.value.aspectRatio,
+                        child: VideoPlayer(controller),
+                      )
+                    : CircularProgressIndicator(),
+              ),
+              Positioned(
+                bottom: 30,
+                left: 20,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.thumb_up, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.bookmark, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.share, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
