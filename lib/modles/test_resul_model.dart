d15ac1ccb735f8344953e179e90a76fba7743444
rahulrajput19614-prog <rahulrajput19614@gmail.com>
2025-08-14 17:06:57 +0530

    Create test_resul_model.dart

---
+++ lib/modles/test_resul_model.dart
@@ -0,0 +1,11 @@
+class QuestionResult {
+  final String questionText;
+  final String userAnswer;
+  final bool isCorrect;
+
+  QuestionResult({
+    required this.questionText,
+    required this.userAnswer,
+    required this.isCorrect,
+  });
+}
