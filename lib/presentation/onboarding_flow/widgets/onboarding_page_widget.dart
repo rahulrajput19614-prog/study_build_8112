2c3de7176c9949a4b4279ec25050d63047b2d516
rahulrajput19614-prog <rahulrajput19614@gmail.com>
2025-08-17 19:12:06 +0530

    Update onboarding_page_widget.dart

--- lib/presentation/onboarding_flow/widgets/onboarding_page_widget.dart
+++ lib/presentation/onboarding_flow/widgets/onboarding_page_widget.dart
@@ -1,70 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../../core/app_export.dart';
+import '../../../models/onboarding_content.dart';
 
 class OnboardingPageWidget extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String description;
+  final OnboardingContent content;
 
-  const OnboardingPageWidget({
-    Key? key,
-    required this.imageUrl,
-    required this.title,
-    required this.description,
-  }) : super(key: key);
+  const OnboardingPageWidget({
+    Key? key,
+    required this.content,
+  }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 6.w),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 60.h,
-            width: 85.w,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 20,
-                  offset: const Offset(0, 10),
-                ),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: CustomImageWidget(
-                imageUrl: imageUrl,
-                width: 85.w,
-                height: 60.h,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          SizedBox(height: 4.h),
-          Text(
-            title,
-            style: AppTheme.lightTheme.textTheme.headlineMedium?.copyWith(
-              fontWeight: FontWeight.w700,
-              color: AppTheme.lightTheme.colorScheme.onSurface,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(height: 2.h),
-          Text(
-            description,
-            style: AppTheme.lightTheme.textTheme.bodyLarge?.copyWith(
-              color: AppTheme.lightTheme.colorScheme.onSurfaceVariant,
-              height: 1.5,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ],
-      ),
-    );
-  }
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 4.w),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            content.image,
+            height: 40.h,
+            fit: BoxFit.contain,
+          ),
+          SizedBox(height: 5.h),
+          Text(
+            content.title,
+            style: GoogleFonts.inter(
+              fontSize: 22.sp,
+              fontWeight: FontWeight.w700,
+              color: AppTheme.textPrimaryLight,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 2.h),
+          Text(
+            content.description,
+            style: GoogleFonts.roboto(
+              fontSize: 14.sp,
+              color: AppTheme.textSecondaryLight,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
 }
