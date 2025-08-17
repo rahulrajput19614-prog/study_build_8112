fa7c928c1c62e07c3346c606c7db075d4ef56b6a
rahulrajput19614-prog <rahulrajput19614@gmail.com>
2025-08-17 19:53:24 +0530

    Create onboarding_content.dart

---
+++ lib/presentation/onboarding_flow/models/onboarding_content.dart
@@ -0,0 +1,11 @@
+class OnboardingContent {
+  final String imageUrl;
+  final String title;
+  final String description;
+
+  OnboardingContent({
+    required this.imageUrl,
+    required this.title,
+    required this.description,
+  });
+}
