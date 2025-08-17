fd0cb3fbb1c6a88ffa1dba0120c97a3f678e0fd5
rahulrajput19614-prog <rahulrajput19614@gmail.com>
2025-08-17 17:49:35 +0530

    Update onboarding_page_widget.dart

--- lib/presentation/onboarding_flow/widgets/onboarding_page_widget.dart
+++ lib/presentation/onboarding_flow/widgets/onboarding_page_widget.dart
@@ -30,7 +30,7 @@ class OnboardingPageWidget extends StatelessWidget {
               borderRadius: BorderRadius.circular(16),
               boxShadow: [
                 BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
+                  color: Colors.black.withValues(alpha: (0.1 * 255).toInt()),
                   blurRadius: 20,
                   offset: const Offset(0, 10),
                 ),
